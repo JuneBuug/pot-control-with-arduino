2f0cb828b3b4bd0e4b3e77267da985e2b6eb1fe1
Ubuntu <ubuntu@ip-172-31-29-40.ap-northeast-2.compute.internal>
2018-06-07 08:01:44 +0000

    [Server] production file settings changed

--- API/API/settings/prod.py
+++ API/API/settings/prod.py
@@ -3,8 +3,9 @@ import json
 DEBUG = False
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
 path = os.path.join(BASE_DIR, 'envs.json')
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 with open(path) as f:
     dict = json.load(f)
     SECRET_KEY = dict["SECRET_KEY"]
